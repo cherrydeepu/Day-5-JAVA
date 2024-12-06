e40f33d610977972d082de2086900a8a91f3be11
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-06 19:29:27 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class pattern1{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println( );
+          }  
+    }
+}
