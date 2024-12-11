12f40355724e8c78dfbc99b6be47ed7969e0ad66
Rishitha <trishithapandu2218@gmail.com>
2024-12-11 05:52:18 +0530

    Add files via upload

---
+++ SingleArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SingleArray{
+    public static void main(String args[]){
+        //initialization
+        int arr[]={1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        //accessing
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        //modification
+        arr[2]=2218;
+        System.out.println(Arrays.toString(arr));
+    }
+}
